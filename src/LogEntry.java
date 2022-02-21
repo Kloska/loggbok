adda111dbc39910d04bbb14379d873d0eb813291
Kloska <56342396+Kloska@users.noreply.github.com>
2022-02-21 09:27:11 +0100

    Scanner, arraylist start

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -1,18 +1,30 @@
 import java.io.Serializable;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 public class LogEntry implements Serializable{
     private Date createdAt;
     private Date updatedAt;
     private String message;
-    private String author;
     // Optional ArrayList<LogEntry> history;
 
     public LogEntry(String message) {
         this.message = message;
         createdAt = new Date();
         updatedAt = new Date();
+    }
+
+    //gör till arraylist och spara till fil
+    public ArrayList input(){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Skriv in ditt namn, tryck enter och skriv sedan din log för idag");
+        String name = myObj.nextLine();
+        String log = myObj.nextLine();
+        // test
+        System.out.println("Datum" + );
+        System.out.println("Namn: " + name);
+        System.out.println("Log: " + log);
     }
 
     public Date getCreatedAt() {
